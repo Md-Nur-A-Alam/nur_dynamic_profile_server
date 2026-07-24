bc45ab31a2bafd09fc9c5bb871ee72bc4e30d126
Md-Nur-A-Alam <mdnuralam2812@gmail.com>
2026-07-25 00:10:37 +0600

    update the vercel and index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./server.js');
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
+
+// Initialize database connection for serverless function
+connectDB().catch(console.error);
+
+// Export the express app for Vercel to use as a serverless function
+module.exports = app;
